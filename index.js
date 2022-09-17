d19ac44a5f9c259aa257e8da448ba88c3293d149
Hemanth Kumar <khemanthkumar360@gmail.com>
2022-09-17 08:51:57 +0000

    sql running in nodeJs

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+
+const dbPath = path.join(__dirname, "goodreads.db");
+
+let db = null;
+
+const initializeDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3001, () => {
+      console.log("Server Running");
+    });
+  } catch (e) {
+    console.log(`db Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDbAndServer();
